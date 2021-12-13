0701559e573a32e01284988cc35d451603814074
Yatish Kelkar <yatish1606@gmail.com>
2021-12-13 13:31:50 +0530

    Update

--- frontend/src/helpers/uploadVideo.ts
+++ frontend/src/helpers/uploadVideo.ts
@@ -4,13 +4,30 @@ import { API_URL } from '../constants'
 const uploadVideo = (video: File): void => {
 
     const path = API_URL + 'file/upload'
-    axios.post(
-        path, 
-        {
-            file: video
+    let body: FormData = new FormData()
+    body.append('file', video)
+    // axios.post(
+    //     path, 
+    //     {
+    //         file: new FormData().append('file', video)
+    //     }, 
+    //     {
+    //         headers: {
+    //             'Access-Control-Allow-Origin': 'http://localhost:3000'
+    //         }
+    //     }
+    // )
+    // .then(response => console.log(response))
+    // .catch(error => console.log(error))
+    fetch(
+        path, {
+            method: 'POST',
+            body: body,
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            }
         }
-    )
-    .then(response => console.log(response))
+    ).then(response => console.log(response))
     .catch(error => console.log(error))
 }
 
