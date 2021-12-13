a979a95b8e2c441616abb64ebdc84ddc6e63029c
Yatish Kelkar <yatish1606@gmail.com>
2021-12-13 17:32:26 +0530

    Video upload works

--- frontend/src/helpers/uploadVideo.ts
+++ frontend/src/helpers/uploadVideo.ts
@@ -2,19 +2,14 @@ import axios, { AxiosResponse } from 'axios'
 import { API_URL } from '../constants'
 
 const uploadVideo = (video: any): Promise<AxiosResponse<any,any>> => {
-    
+    console.log(video)
     const path = API_URL + 'file/upload'
     let body: FormData = new FormData()
     body.append('file', video)
     console.log(body)
     return axios.post(
         path, 
-        null, 
-        {
-            params: {
-                file: new FormData().append('file', video)
-            }
-        }
+        body
     )
 }
 
